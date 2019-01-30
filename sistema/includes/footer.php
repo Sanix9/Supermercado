038b633a0ee6ec726da607dbcece84d996f20d4d
Sanix <Sanix@172.16.216.218>
2019-01-30 14:37:12 -0500

    foorter

--- sistema/includes/footer.php
+++ sistema/includes/footer.php
@@ -1,2 +1,3 @@
-
-
+<footer id="page-foorter">
+    &copy; 2019 Programacion Hyeprmedial
+</footer>
--- sistema/lista_usuarios.php
+++ sistema/lista_usuarios.php
@@ -1,5 +1,5 @@
 <?php
-    include '../conexion.php';
+include '../conexion.php';
 ?>
 
 
@@ -8,12 +8,12 @@
 <html lang="es">
     <head>
         <meta charset="UTF-8">
-        <?php include 'includes/scripts.php';?>
+        <?php include 'includes/scripts.php'; ?>
         <title>Lista de Usuarios_Supermercado</title>
-        
+
     </head>
     <body>
-        <?php include 'includes/header.php';?>
+        <?php include 'includes/header.php'; ?>
         <section id="container">
             <h1>Lista de usuarios</h1>
             <a href="registro_usuario.php" class="btn_new">Crear Usuario</a>
@@ -25,40 +25,41 @@
                     <th>Usuario</th>
                     <th>Rol</th>
                     <th>Acciones</th>
-                    
+
                 </tr>
-                
-                <?php 
-                $query = mysqli_query($conection,"SELECT u.idusuario, u.nombre, u.correo, u.usuario, r.rol FROM usuario u INNER JOIN rol r ON u.rol=r.idrol WHERE estatus = 1");
-                
-                $result= mysqli_num_rows($query);
-                if ($result> 0) {
+
+                <?php
+                $query = mysqli_query($conection, "SELECT u.idusuario, u.nombre, u.correo, u.usuario, r.rol FROM usuario u INNER JOIN rol r ON u.rol=r.idrol WHERE estatus = 1");
+
+                $result = mysqli_num_rows($query);
+                if ($result > 0) {
                     while ($data = mysqli_fetch_array($query)) {
-                 ?>      
-                     
-                  <tr>
-                    <td><?php echo $data["idusuario"]; ?></td>
-                    <td><?php echo $data["nombre"]; ?></td>
-                    <td><?php echo $data["correo"]; ?></td>
-                    <td><?php echo $data["usuario"]; ?></td>
-                    <td><?php echo $data["rol"]; ?></td>
-                    <td>
-                        <a class="link_edit" href="editar_usuario.php?id=<?php echo $data["idusuario"]; ?>">Editar</a>
-                        
-                        <?php if($data["idusuario"] != 1) {?>
-                        |
-                        <a class="link_delete" href="eliminar_confirmar_usuario.php?id=<?php echo $data["idusuario"]; ?>">Eliminar</a>
-                        <?php } ?>
-                    </td>
-                    
-                </tr>
-                   <?php       
+                        ?>      
+
+                        <tr>
+                            <td><?php echo $data["idusuario"]; ?></td>
+                            <td><?php echo $data["nombre"]; ?></td>
+                            <td><?php echo $data["correo"]; ?></td>
+                            <td><?php echo $data["usuario"]; ?></td>
+                            <td><?php echo $data["rol"]; ?></td>
+                            <td>
+                                <a class="link_edit" href="editar_usuario.php?id=<?php echo $data["idusuario"]; ?>">Editar</a>
+
+                                <?php if ($data["idusuario"] != 1) { ?>
+                                    |
+                                    <a class="link_delete" href="eliminar_confirmar_usuario.php?id=<?php echo $data["idusuario"]; ?>">Eliminar</a>
+                                <?php } ?>
+                            </td>
+
+                        </tr>
+                        <?php
                     }
                 }
                 ?>
-                
+
             </table>
         </section>
-            
-    </body>
-</html>
+
+        <?php include "includes/footer.php"; ?>
+    </body> 
+</html> 
